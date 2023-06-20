8278ecb160b98d4718f0cf04cda82e4e940baedf
TheYayra01 <108099019+TheYayra01@users.noreply.github.com>
2023-06-20 21:29:20 +0000

    Create the react file and add the splashscreen and the welcome page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import SplashScreen from './Components/SplashScreen';
+import Welcome from './Components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <SplashScreen />
+      {/* <Welcome /> */}
+    </div>
+  );
+}
+
+export default App;
