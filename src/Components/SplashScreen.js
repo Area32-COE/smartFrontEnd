4a7b545d7eb28a70c20c5ba95baa409ea79d2349
Adaks <adakueli9@gmail.com>
2023-06-26 03:06:03 +0000

    I am done with the splash screen. The only thing to change will be the walking toucan

--- src/Components/SplashScreen.js
+++ src/Components/SplashScreen.js
@@ -1,16 +1,19 @@
 import classes from "./SplashScreen.module.css";
 import splashScreen from "../Images/wlakingToucan.gif";
+import { Link } from "react-router-dom";
 
 function SplahScreen() {
   return (
-    <div className={classes.pageContainer}>
-      <img
-        className={classes.centeredImage}
-        src={splashScreen}
-        alt="my-gif"
-      ></img>
-      <h1 className={classes.welcomeName}>SmarTFleet</h1>
-    </div>
+    <Link to="/welcome">
+      <div className={classes.pageContainer}>
+        <img
+          className={classes.centeredImage}
+          src={splashScreen}
+          alt="my-gif"
+        ></img>
+        <h1 className={classes.welcomeName}>SMARTFLEET</h1>
+      </div>
+    </Link>
   );
 }
 
